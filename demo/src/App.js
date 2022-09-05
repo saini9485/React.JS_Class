d74c210abb6bcdc22b2c0393396fbb89fb5d4aa0
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-09-05 22:20:41 +0530

    React Class

---
+++ demo/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Component1 } from "./components/Component1";
+function App() {
+  return (
+    <div>
+      <Component1></Component1>
+      <section>
+      <h1>hello</h1>
+      <h2>This is my First React Class</h2>
+      </section>
+    </div>
+  );
+}
+export default App;
---
+++ demo/src/components/Component1.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+export class Component1 extends Component {
+    render(){
+        return (
+            <div>
+                <h1> Happy Today React Class </h1>
+                <h2> I am happy to class </h2>
+            </div>
+        )
+    }
+}
