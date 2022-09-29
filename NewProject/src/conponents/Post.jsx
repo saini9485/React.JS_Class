42a664303b30f14e09d884707a89c8608ecf9ec4
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-09-29 21:15:55 +0530

    Test

--- NewProject/src/conponents/Post.jsx
+++ NewProject/src/conponents/Post.jsx
@@ -8,45 +8,44 @@ export function PostTest() {
       .get(`https://jsonplaceholder.typicode.com/comments`)
       .then((response) => {
         console.log(response.data);
-        setPost([...response.data]);
+        setPost([...response.data]); 
       });
   }, []);
 
-  // const handleChange = (e) => {
-  //   setSearch(e.target.value);
-  // };
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-  // const filteredPost = post.filter((posts) =>
-  //   posts.name.toLowerCase().includes(search.toLowerCase())
-  // );
+  const filteredPost = post.filter((posts) =>
+    posts.name.toLowerCase().includes(search.toLowerCase())
+  );
 
    return (
-    <h1>Text </h1>
-  //   // <div className="Post-app">
-  //   //   <div className="Post-search">
-  //   //     <h1 className="Post-text">Search </h1>
-  //   //     <form>
+    // <h1>Text </h1>
+    <div className="Post-app">
+      <div className="Post-search">
+        <h1 className="Post-text">Search </h1>
+        <form>
   
-          // <input
-          //   className="Post-input"
-          //   type="text"
-          //   onChange={handleChange}
-          //   placeholder="Search"
-          ///>
-  //   //       <button className="Search_Btn">Search Post</button>
-  //   //     </form>
-  //   //   </div>
-  //   //   
-  //   //     return (
-  //   //       <>
-                //{filteredPost.map((post) => {
-  //   //         key={post.id}
-  //   //         name={post.name}
-  //   //         email={post.email} 
-  //   //         body={post.body}
-  //   //       </>
-  //   //     );
-  //   //   })}
-  //  // </div>
+          <input
+            className="Post-input"
+            type="text"
+            onChange={handleChange}
+            placeholder="Search"
+          />
+          <button className="Search_Btn">Search Post</button>
+       </form>
+       </div>
+        {filteredPost.map((post) => {
+         return (
+           <div>
+            key={post.id}
+            name={post.name}
+            email={post.email} 
+            body={post.body}
+          </div>
+        );
+      })}
+   </div>
    )
 }
