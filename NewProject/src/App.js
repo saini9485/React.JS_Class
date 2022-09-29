4bf6fe67fc5657f0a5ae4c9b25cdac0278d9e018
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-09-29 21:09:19 +0530

    Test

---
+++ NewProject/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { PostTest } from './conponents/Post';
+
+export  function App() {
+  return (
+    <div>
+<PostTest/>
+    
+    </div>
+  )
+}
+
+export default App;
+
