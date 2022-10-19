0088122e646d51d03c758191b7f164b3e61c58bb
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-10-19 21:56:51 +0530

    CounterApp

--- functinalComponetClass/src/componets/CounterAssignment/Counter.jsx
+++ functinalComponetClass/src/componets/CounterAssignment/Counter.jsx
@@ -1,23 +1,85 @@
- import React from 'react'
- import { useState, useEffect } from 'react'
+//  import React from 'react'
+//  import { useState, useEffect } from 'react'
+// export  function Counter() {
+//    const [count, setCount] = useState(0)
+//    const [btn, setBtn] = useState("Increament")
+//   useEffect(() => {
+//     // document.title = `You clicked ${count} times`;
+//    (count===10)?
+//  setBtn("Decreament"):setBtn("increament")
+
+//   } ,[count]);
+
+// const increament = ()=>{
+//   setCount(count+1)
+
+// }
+// const Decreament = ()=>{
+//   setCount(count-1)
+// }
+// const {handleClick = ()=>{
+//   (btn)? increament():Decreament()
+// }
+
+//   return (
+//     <div>
+//      <h1>CounterAssignment</h1>
+//      <h1>Count {count}</h1>
+//      <div className='btn' >
+//      <button onClick={handleClick}>Increament</button>
+//      </div>
+//       </div>
+//   )
+// }
+
+import React from "react";
+import { useState } from "react";
 export  function Counter() {
-   const [count, setCount] = useState(0)
-  useEffect(() => {
-    document.title = `You clicked ${count} times`; 
-   if(count>10){
-    setCount(count-1)
-   }
-  });
- 
+  let [count, setCount] = useState(0);
+  let [state, setState] = useState(true);
+  let incrementCounter = () => setCount(count + 1);
+  let decrementCounter = () => setCount(count - 1);
+  
+  let handleCount = () => {
+    if (state === true) {
+      if (count < 10) {
+        incrementCounter();
+      }
+      if (count === 10) {
+        decrementCounter();
+        setState(false);
+        document.querySelector("button").innerHTML = "decrement";
+      }
+    }
+    if (state === false) {
+      if (count < 10) {
+        decrementCounter();
+      }
+      if (count === 0) {
+        incrementCounter();
+        setState(true);
+        document.querySelector("button").innerHTML = "Increment";
+      }
+    }
+  };
   return (
-    <div>
-     <h1>CounterAssignment</h1> 
-     <h1>Count {count}</h1>
-     <div className='btn' >
-     <button onClick={()=>setCount(count+1)}>Increament</button>
-     </div>
-      </div>
-  )
+    <div className="btn">
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          handleCount();
+        }}
+      >
+        Increment
+      </button>
+      <button
+        className="name"
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        reset
+      </button>
+    </div>
+  );
 }
-
-
